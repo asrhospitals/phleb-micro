4d32de039107630210cc8073995e3ea755015836
MamunCh0wdhury <189634349+asrhospitals@users.noreply.github.com>
2025-09-18 20:38:22 +0530

    Get Test Api modified

--- controller/getTestCode.js
+++ controller/getTestCode.js
@@ -1,4 +1,5 @@
 const { Investigation } = require("../model/associationModels/associations");
+const { Op } = require("sequelize");
 
 // 1. Get Short Code Of Investigations
 
@@ -25,6 +26,49 @@ const getShortCodes = async (req, res) => {
   }
 };
 
+const searchTest = async (req, res) => {
+  try {
+    /* 1. Authorization */
+    const { role } = req.user;
+    if (
+      role?.toLowerCase() !== "phlebotomist" &&
+      role?.toLowerCase() !== "admin"
+    ) {
+      return res.status(403).json({
+        message:
+          "Access denied. Only phlebotomists and admins can access this resource.",
+      });
+    }
+
+    /* 2. Query Parameters */
+    const { shortcodes, testname } = req.query;
+    const filters = {};
+
+    if (shortcodes) {
+      filters["shortcode"] = parseInt(shortcodes);
+    }
+    if (testname) {
+      filters["testname"] = {
+        [Op.iLike]: `%${testname}%`,
+      };
+    }
+
+    /* Find Patients Matching the Query */
+    const test = await Investigation.findAll({
+      where: filters,
+      order: [["id", "ASC"]],
+      attributes: ["id", "testname", "shortcode", "normalprice"],
+    });
+
+    return res.status(200).json(test);
+  } catch (error) {
+    return res.status(500).json({
+      message: `Something went wrong while searching tests ${error}`,
+    });
+  }
+};
+
 module.exports = {
   getShortCodes,
+  searchTest,
 };
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,14 @@
 const Router = require("express");
 const { updateTestStatus } = require("../controller/updateTestStatus");
 const { addPatient, createPatient } = require("../controller/addPatient");
-const { getPatient, fetchPatient, searchPatient, getPatientByMobile } = require("../controller/getPatient");
+const {
+  getPatient,
+  fetchPatient,
+  searchPatient,
+  getPatientByMobile,
+} = require("../controller/getPatient");
 const { updatePatientInfo } = require("../controller/updatePatient");
-const { getShortCodes } = require("../controller/getTestCode");
+const { searchTest } = require("../controller/getTestCode");
 
 const router = Router();
 
@@ -13,25 +18,25 @@ router.route("/add-patient-test").post(addPatient);
 // 2.  Add Patient Without Tests for Admin
 router.route("/create-patient").post(createPatient);
 
-// 2. Get Patient Test Data
+// 3. Get Patient Test Data
 router.route("/get-patient-test/:id").get(getPatient);
 
-// 3. Update Patient Infographic Data
+// 4. Update Patient Infographic Data
 router.route("/update-patient-infographic/:patient_id").put(updatePatientInfo);
 
-// 4. Send Test to Respective Node
+// 5. Send Test to Respective Node
 router.route("/send-tests").put(updateTestStatus);
 
-// 5. Get Short Code
-router.route("/get-shortcodes").get(getShortCodes);
-
 // 6. Get General Patient Info
-router.route('/get-info/:hospitalname').get(fetchPatient);
+router.route("/get-info/:hospitalname").get(fetchPatient);
 
 // 7. Search Patient Data
 router.route("/search-patient").get(searchPatient);
 
 // 8. Get Patient By Mobile Number
 router.route("/get-data-mobile").get(getPatientByMobile);
+
+// 9. Search Test by Test name or Shortcode
+router.route("/search-test").get(searchTest);
 
 module.exports = router;
